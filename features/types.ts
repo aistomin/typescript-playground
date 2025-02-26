f0ba97182d3a16eb37944f82ec33d78dc86a1ad7
aistomin <andrej.istomin.ikeen@gmail.com>
2025-02-26 05:18:21 +0100

    Typescript Course. Section 2.

---
+++ features/types.ts
@@ -0,0 +1,14 @@
+const today = new Date();
+
+const person = {
+    name: 'John',
+    age: 30,
+    email: 'john@example.com',
+    isAdmin: false,
+};
+
+class Color {
+
+}
+
+const color = new Color();
